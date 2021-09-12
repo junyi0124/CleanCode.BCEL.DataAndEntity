191046c3583a8a2a989fbdc2ac1239023e16e649
junyi <junyi0124@msn.com>
2021-09-12 15:06:35 +0800

    v1.1

--- CleanCode.BCEL.DataAndEntity/PagedDataExtensions.cs
+++ CleanCode.BCEL.DataAndEntity/PagedDataExtensions.cs
@@ -9,32 +9,50 @@ namespace CleanCode.BCEL.DataAndEntity
 {
     public static class PagedDataExtensions
     {
-        public static async Task<IPagedData<T>> ToPagedDataAsync<T>(this IQueryable<T> superset, int? pageNumber, int pageSize,
+        /// <summary>
+        /// get paged data function
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="superset"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<PagedMetadata<T>> ToPagedDataAsync<T>(this IQueryable<T> superset, 
+            int? pageNumber, int pageSize,
             CancellationToken cancellationToken)
         {
+            // check page number validation
             if (pageNumber < 1)
             {
-                throw new ArgumentOutOfRangeException($"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
+                throw new ArgumentOutOfRangeException(
+                    $"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
             }
-
+            // check page size validation
             if (pageSize < 1)
             {
-                throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+                throw new ArgumentOutOfRangeException(
+                    $"pageSize = {pageSize}. PageSize cannot be less than 1.");
             }
+            // ensure page number
             int pn = pageNumber ?? 1;
-
+            // paged data collection
             var subset = new List<T>();
             var totalCount = 0;
 
             if (superset != null)
             {
+                // trigger first query
                 totalCount = superset.Count();
                 if (totalCount > 0)
                 {
                     subset.AddRange(
-                        (pageNumber == 1)
-                            ? await superset.Skip(0).Take(pageSize).ToListAsync(cancellationToken).ConfigureAwait(false)
-                            : await superset.Skip(((pn - 1) * pageSize)).Take(pageSize)
+                        //(pn == 1) ? 
+                        //    await superset.Skip(0).Take(pageSize).ToListAsync(cancellationToken)
+                        //        .ConfigureAwait(false)
+                        //    : await superset.Skip(((pn - 1) * pageSize)).Take(pageSize)
+                        //        .ToListAsync(cancellationToken).ConfigureAwait(false)
+                        await superset.Skip((pn - 1) * pageSize).Take(pageSize)
                             .ToListAsync(cancellationToken).ConfigureAwait(false)
                     );
                 }
@@ -43,7 +61,15 @@ namespace CleanCode.BCEL.DataAndEntity
             return new PagedMetadata<T>(subset, pn, pageSize, totalCount);
         }
 
-        public static Task<IPagedData<T>> ToPagedDataAsync<T>(this IQueryable<T> superset, int pageNumber = 1, int pageSize = 10)
+        /// <summary>
+        /// shorter version of ToPagedDataAsync<typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="superset"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static Task<PagedMetadata<T>> ToPagedDataAsync<T>(this IQueryable<T> superset, int pageNumber = 1, int pageSize = 10)
         {
             return superset.ToPagedDataAsync(pageNumber, pageSize, CancellationToken.None);
         }
