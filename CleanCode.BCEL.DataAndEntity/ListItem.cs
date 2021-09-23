2be6e6f8c3208fce361a2e01b5172c368da8b6b3
junyi.zhang <junyi0124@msn.com>
2021-09-23 15:52:15 +0800

    feat: add list model

---
+++ CleanCode.BCEL.DataAndEntity/ListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.BCEL.DataAndEntity
+{
+    public class ListItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public ListItem(string text, string value)
+        {
+            Text = text;
+            Value = value;
+        }
+    }
+}
